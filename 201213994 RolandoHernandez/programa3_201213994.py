ed8a6bb34c05b75d099cdf64d77543167d6e11b2
Rolando Hernández <rolandohernandez91r@gmail.com>
2023-08-25 22:25:28 -0600

    Add: appDemiPreferencia .py

---
+++ 201213994 RolandoHernandez/programa3_201213994.py
@@ -0,0 +1,10 @@
+print("ingrese 4 números enteros")
+lista_numeros = []
+
+for i in range(4):
+    numero=int(input(f"Ingrese el número {i+1}: "))
+    lista_numeros.append(numero)
+
+mayor = max(lista_numeros)
+
+print("El número mayor es: ", mayor)
